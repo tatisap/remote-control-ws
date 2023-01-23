c14447ec6757719ff83ecdaca2fe22081fa3ab30
Tatsiana Sapun <tatiana.sapun@gmail.com>
2023-01-23 12:28:01 +0100

    feat: add router

---
+++ src/app.enum.ts
@@ -0,0 +1,11 @@
+export enum WS_CMD {
+  MOUSE_UP = 'mouse_up',
+  MOUSE_DOWN = 'mouse_down',
+  MOUSE_LEFT = 'mouse_left',
+  MOUSE_RIGHT = 'mouse_right',
+  MOUSE_POSITION = 'mouse_position',
+  DRAW_CIRCLE = 'draw_circle',
+  DRAW_RECTANGLE = 'draw_rectangle',
+  DRAW_SQUARE = 'draw_square',
+  PRNT_SCRN = 'prnt_scrn',
+}
---
+++ src/app.router.ts
@@ -0,0 +1,30 @@
+import { WS_CMD } from './app.enum';
+import { DrawingController } from './drawing/drawing.controller';
+import { ImageController } from './image/image.controller';
+import { MouseController } from './mouse/mouse.controller';
+
+export const appRouter = async (command: string, ...args: number[]): Promise<string> => {
+  switch (command) {
+    case WS_CMD.MOUSE_UP:
+      return MouseController.moveUp(...(args as [number]));
+    case WS_CMD.MOUSE_DOWN:
+      return MouseController.moveDown(...(args as [number]));
+    case WS_CMD.MOUSE_LEFT:
+      return MouseController.moveLeft(...(args as [number]));
+    case WS_CMD.MOUSE_RIGHT:
+      return MouseController.moveRight(...(args as [number]));
+    case WS_CMD.MOUSE_POSITION:
+      return MouseController.getPosition();
+    case WS_CMD.DRAW_CIRCLE:
+      return DrawingController.drawCircle(...(args as [number]));
+    case WS_CMD.DRAW_RECTANGLE:
+      return DrawingController.drawRestangle(...(args as [number, number]));
+    case WS_CMD.DRAW_SQUARE:
+      return DrawingController.drawSquare(...(args as [number]));
+    case WS_CMD.PRNT_SCRN:
+      return ImageController.printScreen();
+    default:
+      break;
+  }
+  return '';
+};
